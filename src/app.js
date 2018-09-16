3fb44f2a937b79b80a764cc6928bdc9d86aa2e16
Todd Roberts <fit2def.user@gmail.com>
2018-09-16 18:53:39 -0500

    added profile view and route and account-specific routes that link to the account page

--- src/app.js
+++ src/app.js
@@ -7,13 +7,39 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 
+    'utf8'
+);
+
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 
+    'utf8'
+);
+
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Index'});
+    res.render('index', { title: 'Account Summary', accounts });
 });
 
-// app.get('/profile', (req, res) => {
-//     res.render('profile')
-// });
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] });
+});
 
 // app.get('/transfer', (req, res) => {
 //     res.render('transfer')
